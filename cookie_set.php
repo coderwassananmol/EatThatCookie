758d55c13ef7483cec44a8801220045ff1085011
coderwassananmol <wassan.anmol@gmail.com>
2016-06-06 17:12:34 +0530

    Fixed the bug that caused the cookies to be eaten up everytime.

--- cookie_set.php
+++ cookie_set.php
@@ -1,7 +1,8 @@
 <?php
-
-$random = rand(1, 5);
-setcookie('cookie', 'Thanks for eating the cookies.', time() +$random);
+session_start();
+$random = rand(1,5);
+$_SESSION['random']=$random;
+setcookie('cookie', 'Thanks for eating the cookies.', time() +$_SESSION['random']);
 echo '<font size="10" color="blue">Hurry!!!!Click on cookie before it gets all eaten up.</font>';
 echo '<br />';
 echo '<a href="cookie_view.php"> <img src="cookie.jpg" height="200px" width="200px"> </a>';
