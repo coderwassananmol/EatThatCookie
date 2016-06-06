25b8901839c561ff1632b6d614a953bb9915cf75
coderwassananmol <wassan.anmol@gmail.com>
2016-06-06 17:30:18 +0530

    Added comments

--- cookie_set.php
+++ cookie_set.php
@@ -1,8 +1,9 @@
 <?php
+
 session_start();
 $random = rand(1,5);
-$_SESSION['random']=$random;
-setcookie('cookie', 'Thanks for eating the cookies.', time() +$_SESSION['random']);
+$_SESSION['random']=$random; //Session variable to fix the random number to the server.
+setcookie('cookie', 'Thanks for eating the cookies.', time() +$_SESSION['random']); //Cookie set up with a random expiry.
 echo '<font size="10" color="blue">Hurry!!!!Click on cookie before it gets all eaten up.</font>';
 echo '<br />';
 echo '<a href="cookie_view.php"> <img src="cookie.jpg" height="200px" width="200px"> </a>';
